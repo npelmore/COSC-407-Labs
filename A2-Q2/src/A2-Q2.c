9f24850c0315096157b404b6605aadcdb5dbd31e
npelmore <nate.b.pelmore@gmail.com>
2022-09-28 12:40:22 -0700

    finished A2 Q1 Q2 Q3

--- A2-Q2/src/A2-Q2.c
+++ A2-Q2/src/A2-Q2.c
@@ -1,6 +1,6 @@
 /*
  ============================================================================
- Name        : A2-Q1.c
+ Name        : A2-Q2.c
  Author      : Nathan Pelmore
  ============================================================================
  */
@@ -12,21 +12,18 @@
 int main(void) {
 	clock_t start_t, end_t;
 	double total_t;
-
-	void addVec(int* C, int* A, int* B, int size);
-
+	int* addVec2(int* A, int* B, int size);
 	int size = 50000000;
 	int* A = calloc(size, sizeof(int));
 	int* B = calloc(size, sizeof(int));
-	int* C = calloc(size, sizeof(int));
 
-	if(A==NULL || B==NULL || C==NULL){
-			fprintf(stderr, "Not Enough Memory\n");
-			exit(EXIT_FAILURE);
+	if(A==NULL || B==NULL){
+		fprintf(stderr, "Not Enough Memory\n");
+		exit(EXIT_FAILURE);
 	}
 
 	start_t = clock();
-	addVec(C,A,B,size);
+	int* C = addVec2(A,B,size);
 	end_t = clock();
 
 	total_t = (double)(end_t - start_t);
@@ -42,14 +39,20 @@ int main(void) {
 	return EXIT_SUCCESS;
 }
 
-void addVec(int* C, int* A, int* B, int size){
-	for(int i=0;i<size;i++){
-		C[i] = A[i] + B[i];
+int* addVec2(int* A, int* B, int size){
+	int* C = calloc(size, sizeof(int));
+	if(C==NULL){
+		fprintf(stderr, "Not Enough Memory\n");
+		exit(EXIT_FAILURE);
 	}
+	for(int i=0;i<size;i++){
+			C[i] = A[i] + B[i];
+	}
+	return C;
 }
 
 /*
 Result:
-50 Million = 413.0 ms
+50 Million = 364.0 ms
 
  */
---
+++ A2-Q3/src/A2-Q3.c
@@ -0,0 +1,29 @@
+/*
+ ============================================================================
+ Name        : A2-Q3.c
+ Author      : Nathan PelmoreS
+ ============================================================================
+ */
+#include <omp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	int numThreads, tid;
+	/* This creates a team of threads; each thread has own copy of variables  */
+	#pragma omp parallel private(numThreads, tid)
+	{
+		tid = omp_get_thread_num();
+		printf("Hello World from thread number %d\n", tid);
+
+		/* The following is executed by the master thread only (tid=0) */
+		if (tid == 0) {
+			numThreads = omp_get_num_threads();
+			printf("Number of threads is %d\n", numThreads);
+		}
+	}
+	return 0;
+}
+
+
+
