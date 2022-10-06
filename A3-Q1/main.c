d9a831c9d444bbfd171b415ff7d8da370bd3b17a
npelmore <nate.b.pelmore@gmail.com>
2022-10-06 13:59:49 -0700

    finished Q1 started Q2

--- A3-Q1/main.c
+++ A3-Q1/main.c
@@ -16,7 +16,7 @@ int main() {
 	const char* inFile = "okanagan.bmp";
 	const char* outFile = "okanagan_processed.bmp";
 	const ImgProcessing processingType = desaturate; //or negative
-	int num_thread=2;
+	int num_thread=16;
 
 	UCHAR r, g, b;
 	UINT width, height;
@@ -34,38 +34,33 @@ int main() {
 	double t = omp_get_wtime();
 
 
-#pragma omp parallel num_threads(num_thread)
+#pragma omp parallel num_threads(num_thread) private(x,y,r,g,b)
 	{
-
-
-
-
+		int id = omp_get_thread_num();
+		int sizeOfSection = width/num_thread;
+		int start = id  * sizeOfSection;
+		int end = start + sizeOfSection;
 
 		/* Iterate through all the image's pixels */
-		for (x = 0; x < width; ++x) {
+		for (x = start; x < end; ++x) {
 
 
-			for (int i=0; i<num_thread; i++){
 
+			for (y = 0; y < height; ++y) {
+				/* Get pixel's RGB values */
+				BMP_GetPixelRGB(bmp, x, y, &r, &g, &b);
 
-				for (y = 0; y < height; ++y) {
-					/* Get pixel's RGB values */
-					BMP_GetPixelRGB(bmp, x, y, &r, &g, &b);
-
-					/* Write new RGB values */
-					if(processingType == negative)
-						BMP_SetPixelRGB(bmp, x, y, 255 - r, 255 - g, 255 - b);
-					else if(processingType == desaturate){
-						UCHAR gray = r * 0.3 + g * 0.59 + b * 0.11;
-						BMP_SetPixelRGB(bmp, x, y, gray, gray, gray);
-					}
+				/* Write new RGB values */
+				if(processingType == negative)
+					BMP_SetPixelRGB(bmp, x, y, 255 - r, 255 - g, 255 - b);
+				else if(processingType == desaturate){
+					UCHAR gray = r * 0.3 + g * 0.59 + b * 0.11;
+					BMP_SetPixelRGB(bmp, x, y, gray, gray, gray);
 				}
 			}
 
 		}
 	}
-
-
 
 
 	/* calculate and print processing time*/
@@ -81,4 +76,8 @@ int main() {
 
 	return 0;
 }
-//Finished image processing in 275.0 ms.
+//Sequential: Finished image processing in 797.0 ms.
+//2 Threads:Finished image processing in 632.0 ms.
+//4 Threads:Finished image processing in 431.0 ms.
+//8 Threads:Finished image processing in 384.0 ms.
+//16 Threads:Finished image processing in 385.0 ms.
