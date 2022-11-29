a02278f99884879e0b16a5a0e77151a1587a037d
Nathan Pelmore <nate.b.pelmore@gmail.com>
2022-11-28 22:43:12 -0800

    wip A8

--- A8/reduction.cu
+++ A8/reduction.cu
@@ -19,26 +19,59 @@ void checkForGPU() {
         printf("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
     }
 }
+__device__ int sum;
+//__constant__ int nthreads = 512;
+
+__global__ void reduction1(int* arr){
+    __shared__ float partialSum[512];
+    
+    int i = threadIdx.x;
+
+    for(int k=i; k<blockDim.x; k++)
+        partialSum[k] = arr[k];
+    
+    for (int stride = 1; stride<blockDim.x; stride *= 2){
+        if (i % (2 * stride) == 0)
+            partialSum[i] += partialSum[i + stride];
+        __syncthreads();
+    }
+    sum += partialSum[0];
+
+}
 
 int main() {
     checkForGPU();
-    //int size = pow(2,24);
-    int size = 10;
-    int* arr = (int*) malloc(size * sizeof(int));
-
+    int size = pow(2,24);
+    //int size = 10;
+    int nbytes = size * sizeof(int);
+    int *arr = (int*) malloc(nbytes);
+    int *d_arr;
+    int nthreads = 512;
+    int nblocks = (size-1)/nthreads + 1;
+    
     srand(time(NULL));
     for(int i=0; i<size; i++)
         arr[i] = rand() % 256;
+
+    cudaMalloc(&d_arr, nbytes);
+    cudaMemcpy(d_arr, arr, nbytes, cudaMemcpyHostToDevice);
+
+    dim3 gridSize(nblocks,1,1);
+    dim3 blockSize(nthreads,1,1);
     
+    //cudaMallocManaged(arr, nbytes); 
 
+    reduction1<<<gridSize,blockSize>>>(d_arr);
 
-
-
+    cudaMemcpy(arr, d_arr, nbytes, cudaMemcpyDeviceToHost);
+    
 
     // for(int i=0; i<size; i++)
     //     printf("%d ", arr[i]); 
 
+    printf("\nSum = %d", sum);
     free(arr);    
+    cudaFree(d_arr);
 
     return 0;
 }
